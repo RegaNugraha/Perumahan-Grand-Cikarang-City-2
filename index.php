a5869f0be7b16b60cc3a3709f9ff5e13c65c4735
Ega Rega <reganugraha77@gmail.com>
2021-09-23 00:08:36 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PERUMAHAN GRAND CIKARANG CITY 2</title>
+    <link rel="stylesheet" href="stylesheet.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="header-logo">GRAND CIKARANG CITY 2</div>
+      <div class="header-list">
+        <ul>
+          <li><a href="#about-link">Home</a></li>
+          <li><a href="#work-link">Tipe Rumah</a></li>
+          <li><a href="#contact-link">Hubungi Saya</a></li>
+          <li><a href="#fasilitas-link">Fasilitas</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="copy-container">
+        <h1>PERUMAHAN GRAND CIKARANG CITY 2<span>.</span></h1>
+        <h2>RUMAH HUNIAN MASA DEPAN</h2>
+        <img src="https://id2-cdn.pgimgs.com/listing/18805629/UPHO.107345559.V800/Jual-over-kredit-perum-GCC-2-cikarang-Bekasi-Indonesia.jpg" alt="gerbang">
+      </div>
+      
+      <div class="about">
+        <h3><a id="about-link">Perumahan Grand Cikarang City 2</a></h3>
+        <p>Perumahan Subsidi murah rumah viral Grand Cikarang City 2 (GCC 2) Lokasi sangat strategis dekat dengan sarana pendidikan, kantor pemerintahan, stasiun kereta, pasar tradisional, dan tidak jauh dari jalan propinsi pantura. Terletak di Jl. Kedung Waringin, Bekasi, Jawa Barat atau lebih tepatnya perbatasan antara Bekasi dengan Karawang. Launching perumahan subsidi baru dibuka pada bulan Agustus 2018. Untuk saat ini, pembangunan sudah pada tahap ke dua. – Rumah Viral</p>
+        <h3><a id="about-link">Promo Diskon DP 50%</a></h3>
+        <img src=""alt="Diskon">
+        <h3><a id="about-link">Lokasi/ Alamat Grand Cikarang City 2</a></h3>
+        <p>Lokasi / alamat perumahan Grand Cikarang City 2 berada di jalan pantura Karawang – Bekasi, lebih tepatnya di Kampung Babakan, Ds. Kedung Waringin, Kec. Kedung Waringin, Kab. Bekasi.</p>
+                    
+    </div>
+    </div>
+
+    
+    <div class="contact-form">
+        <h3 class="contact-title"><a id="contact-link">Hubungi Saya</a></h3>
+        <!--<input placeholder="Email (Diperlukan)">
+        <br>
+        <textarea placeholder="Message (Diperlukan)"></textarea>
+        <br>
+        <input class="contact-submit" type="submit" value="Kirim">-->
+        <p>No : 0895 2446 7830</p>
+        <p>Email : reganugraha77@gmail.com</p>
+        <p>Ig : @ega_rega</p>
+        <a href="https://wa.me/qr/BDI7DBAT5HL5H1"><button>Pesan Sekarang</button></a>
+</div>
+</div>   
+
+    <div class="fasilitas-form">
+        <h6 class="fasilitas-title"><a id="fasilitas-link">Fasilitas</a></h6>
+        <p>Rumah Sakit</p>
+        <p>Jalan Tol</p>
+        <p>Sekolah</p>
+        <p>Mall</p>
+        <p>Bank/ ATM</p>
+        
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="footer-logo">PERUMAHAN GRAND CIKARANG CITY 2</div>
+      <div class="footer-list">
+        <ul>
+          <li><a href="#about-link">Home</a></li>
+          <li><a href="#work-link">Tipe Rumah</a></li>
+          <li><a href="#contact-link">Hubungi Saya</a></li>
+          <li><a href="fasilitas-link">Fasilitas</a></li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
